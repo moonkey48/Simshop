217a76420c1020f270ef107d83afa98289576b6c
= <moonkey48@icloud.com>
2022-12-08 13:44:08 +0900

    feature: add products type

---
+++ src/types/products.ts
@@ -0,0 +1,10 @@
+
+export type TProduct = {
+    id:number,
+    title:string, 
+    price:string,
+    description:string,
+    category:string,
+    image:string,
+}
+export type TProducts = TProduct[];
